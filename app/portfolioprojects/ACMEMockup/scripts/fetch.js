fec61357b41e2f549ed413ca57414de04331a0ea
dragansalg <70744244+dragansalg@users.noreply.github.com>
2023-01-06 13:58:19 +0100

    testing fix
    changed:
    fetch('../scripts/events.json')
    to:
    fetch('./scripts/events.json')

--- app/portfolioprojects/ACMEMockup/scripts/fetch.js
+++ app/portfolioprojects/ACMEMockup/scripts/fetch.js
@@ -1,7 +1,7 @@
 //fetching data from external json file and sending it to local storage.
 //will only fetch if local storage is not already defined!
 if (!localStorage.getItem("full_event")) {
-    fetch('../scripts/events.json')
+    fetch('./scripts/events.json')
     .then(response=>response.json())
     .then(data=>{
     // stringify the json and sending data to local storage.
